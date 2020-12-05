dfaabffb5f4144f01f2ae342c9bfbc89b950a6e1
Andrew Czachowski <czachandrew@gmail.com>
2020-12-05 15:39:31 -0600

    added site

---
+++ src/components/Announcement.vue
@@ -0,0 +1,62 @@
+<template>
+  <section class="section">
+    <div class="jumbotron">
+      <h1 class="display-4">The Forrest Country Club Exposed</h1>
+      <p class="lead">
+        This website is to make public the hidden problems and lawsuits of the
+        Forest country club being experience by current and former members.
+      </p>
+      <hr class="my-4" />
+      <p>
+        All public information from Todd Ferguson’s pending filings against the
+        forest country club and the director of golf and the GM specifically
+        will be posted here as well as all discovery and public information
+        regarding the lawsuits.
+      </p>
+      <h2>Update and member forum coming soon!</h2>
+      <a
+        class="btn btn-primary btn-lg"
+        href="email:gavalex@mac.com"
+        role="button"
+        >Contact</a
+      >
+    </div>
+    <div class="row">
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            <p>
+              This site will be set for all current and former members of The
+              Forest Country Club to post their grievances and complaints for
+              all of the Forest members and potential members to see.
+            </p>
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            This site is to make sure the wonderful members of the Forest aren’t
+            hit with SPECIAL ASSESSMENTS because the Director of Golf, and GM
+            have no respect for your money.
+          </div>
+        </div>
+      </div>
+      <div class="col">
+        <div class="card">
+          <div class="card-body">
+            Special assessments are being levied to pay for these unnecessary
+            lawsuits because of two EMPLOYEES - this should not impact the
+            membership.
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+<script lang="js">
+import {defineComponent} from 'vue';
+export default defineComponent({
+    name: 'Announcement'
+})
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="content">
+    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+    <Announcement style="margin-top: 24px;" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from "@/components/HelloWorld.vue";
+import Announcement from "@/components/Announcement.vue";
+
+export default {
+  name: "Home",
+  components: {
+    Announcement
+  }
+};
+</script>
+
+Announcement
